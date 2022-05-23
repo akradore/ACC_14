7b546d6171d3624f9c3b51b24bcd6da22bb18c3e
alphapospty <eferreira@alphapos.biz>
2022-05-23 11:43:14 -0500

    update modules

--- alphabot_kit_anglosaxon/models/__init__.py
+++
@@ -1,5 +0,0 @@
-# -*- coding: utf-8 -*-
-# Part of Softhealer Technologies.
-
-from . import account_move
-from . import account_move_line
--- sh_landed_cost_cancel/doc/changelog.rst
+++
@@ -1,4 +0,0 @@
-14.0.1 (Date : 29th January 2020) 
-----------------------------
- - Initial Release 
- 
